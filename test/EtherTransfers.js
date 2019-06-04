c4d9e8391023adf91191d11ce93a7a4180b3c860
Michał Dutkiewicz <emdeck@gmail.com>
2019-06-04 08:26:47 +0200

    Update transfer related tests

--- test/EtherTransfers.js
+++ test/EtherTransfers.js
@@ -12,6 +12,6 @@ contract('EtherTransfers', function(accounts) {
     const instance = await EtherTransfers.new();
     await instance.sendEther([accounts[1], accounts[2], accounts[3]], [100, 200, 300], {from: accounts[0], value: 600});
 
-    assert.equal([(await web3.eth.getBalance(accounts[1])), (await web3.eth.getBalance(accounts[2])), (await web3.eth.getBalance(accounts[3]))].join(', '), balances.join(', '), 'List of holders is different than [' + balances.join(', ') + ']');
+    assert.sameMembers([(await web3.eth.getBalance(accounts[1])), (await web3.eth.getBalance(accounts[2])), (await web3.eth.getBalance(accounts[3]))], balances, 'List of holders is different than [' + balances.join(', ') + ']');
   });
 });
--- test/TokenTransfers.js
+++ test/TokenTransfers.js
@@ -9,13 +9,13 @@ contract('TokenTransfers', function(accounts) {
   });
   it('check sending tokens', async() => {
     const balances = ['100', '200', '300'];
-    const tokenInstance = await PacificToken.new(['0x0000000000000000000000000000000000000000'], 0);
+    const tokenInstance = await PacificToken.new(['0x0000000000000000000000000000000000000000'], [0]);
     const transfersInstance = await TokenTransfers.new();
 
     await tokenInstance.transfer(transfersInstance.address, 600, {from: accounts[0]});
 
     await transfersInstance.sendToken(tokenInstance.address, [accounts[1], accounts[2], accounts[3]], [100, 200, 300], {from: accounts[0]});
 
-    assert.equal([(await tokenInstance.balanceOf.call(accounts[1])), (await tokenInstance.balanceOf.call(accounts[2])), (await tokenInstance.balanceOf.call(accounts[3]))].join(', '), balances.join(', '), 'List of holders is different than [' + balances.join(', ') + ']');
+    assert.sameMembers([(await tokenInstance.balanceOf.call(accounts[1])).toString(), (await tokenInstance.balanceOf.call(accounts[2])).toString(), (await tokenInstance.balanceOf.call(accounts[3])).toString()], balances, 'List of holders is different than [' + balances.join(', ') + ']');
   });
 });
